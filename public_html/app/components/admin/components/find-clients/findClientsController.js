890c716cccf51ad2c5e07a8be2f74820caa26fdd
mikemeding <mikeymeding@gmail.com>
2015-03-11 13:14:10 -0400

    added census data serching,paging,and ranging. nice table too

--- public_html/app/components/admin/components/find-clients/findClientsController.js
+++ public_html/app/components/admin/components/find-clients/findClientsController.js
@@ -25,14 +25,30 @@
 
         //getAppraisalDataInAssessedRange/{taxYear}/{fromValue}/{toValue}
         $scope.years = ["2013", "2014"]; // currently available tax years
-        $scope.data = {}
+        $scope.selectedYear = "Select Year";
+        /**
+         * When the year is toggled in the dropdown update both the display year and that for the argument
+         * @param choice
+         */
+        $scope.yearToggle = function (choice) {
+            $scope.selectedYear = choice;
+            $scope.data.year = choice;
+        }
 
+        $scope.data = {}
+        $scope.tableData = [];
+        $scope.tableVisibility = false;
+        $scope.sortOrder = "marketValue"; // inital soring order
+
+        /**
+         * This is called on submit of the find clients form.
+         */
         $scope.findClientsInRange = function () {
             console.log($scope.data);
 
             var request = {
                 method: 'GET',
-                url: 'http://www.mikemeding.com/fx/appraisal/getAppraisalDataInAssessedRange/'+$scope.data.year+'/'+$scope.data.fromValue+'/'+$scope.data.toValue
+                url: 'http://www.mikemeding.com/fx/appraisal/getAppraisalDataInAssessedRange/' + $scope.data.year + '/' + $scope.data.fromValue + '/' + $scope.data.toValue
             };
 
             console.log(request);
@@ -40,12 +56,14 @@
                 .success(function (data, status, headers, config, response) { // If call successful
                     console.log("Get data successful");
                     console.log("data: " + data);
-                    // DO SUCCESS STUFF HERE
+                    $scope.tableVisibility = true;
+                    $scope.tableData = data.list;
+
                 })
                 .error(function (data, status, headers, config, response) { // If call fails
                     console.log("Failed to get data");
                     console.log('response: ' + response); // response will contain reason why
-                    // DO FAIL STUFF HERE
+                    // TODO: Alert needed for bad or missing data
                 });
         }
 
